9f4d6352129a8fd52334a6ac58651497bbf91805
shayan <shayankolahi30@gmail.com>
2023-09-23 04:06:41 -0700

    header

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red:'#ddd',
+      },
+    },
+    container: {
+      center: true,
+      padding: '15px',
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1250px',
+      },
+    },
+    fontFamily: {
+      IRANSansWeb: 'IRANSansWeb',
+      IRANSansFaNumBold: 'IRANSansFaNum-Bold'
+    },
+  },
+  plugins: [],
+}
+
